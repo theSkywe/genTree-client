94457a421a7ed02be1db579f45a8dfe9d2f81877
Oleg Orlovsky <orlovsky.we.oleg@gmail.com>
2017-11-19 19:18:43 +0200

    tree render

--- src/components/Node.js
+++ src/components/Node.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from "react-redux"
 import { AddButton, DeleteButton } from "./Buttons"
 import "../styles/Node.css"
+import "../styles/Tree.css"
 
 
 function mapStateToProps(state) {
@@ -11,33 +12,61 @@ function mapStateToProps(state) {
 }
 
 export class NodeContainer extends React.Component {
- 
-  render() {
+
+  nodeRender(item) {
     var imagePath = "../images/"
-    if (this.props.data) {
-      
-      var nodeTemplate = this.props.data.map(function(item) {
-        return (             
-          <div className="NodeContainer" key={item.id}>
-            {item.id > 1 && <DeleteButton nodeID={item.id}/>}      
-            <div className="Node" >      
-             {/* 
-             critical dependency
-             <img className="NodeImage" src={require(item.image)} alt="no_image"/> 
-             */}
-              <img className="NodeImage" src={imagePath + item.image} alt="error"/> 
-              <div className="NodeName">{item.name}</div>
-            </div>
-            <AddButton parentID={item.id}/>
-          </div>
-        )
-    })
+    
+    return (             
+      <div className="NodeContainer">
+        {item.id > 1 && <DeleteButton nodeID={item.id}/>}      
+        <div className="Node" >                 
+          <img className="NodeImage" src={ imagePath.concat(item.image)} alt="error"/> 
+          <div className="NodeName">{item.name}</div>
+        </div>
+        <AddButton parentID={item.id}/>
+      </div>
+    )
+  }
+
+  treeBuilder(p) {
+    if ((p.rgt-p.lft) > 1) {
+      return ( 
+          <li>
+            {this.nodeRender(p)}
+              { <ul>
+                {
+                this.props.data.filter( (obj) => {
+                  if (obj.lft > p.lft && obj.rgt < p.rgt && (obj.depth === (p.depth + 1))) {
+                    return true
+                  }
+                }).map( c => {
+                  return (
+                  
+                    this.treeBuilder(c)
+                     
+                  )
+                })}
+              </ul>}
+          </li>
+      ) 
+    } else {
+      return (     
+          <li>
+            {this.nodeRender(p)}
+          </li>  
+      )
     }
+  }
+
+  render() {
+    
 
     return (
-      <div data={this.props.data}>
-        {nodeTemplate}
-      </div>
+          <div className="tree" data={this.props.data}>
+          <ul>
+            {this.treeBuilder(this.props.data[0])}
+            </ul>
+          </div>
     )
   }
 }
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,11 +1,44 @@
 const initialState = [
     {
         id: 1,
-        name: "You",
+        name: "One",
         image: "you.png",
         lft: 1,
-        rgt: 2
-    }
+        rgt: 11,
+        depth: 0
+    },
+    {
+        id: 2,
+        name: "Two",
+        image: "you.png",
+        lft: 3,
+        rgt: 4,
+        depth: 1
+    },
+    {
+        id: 3,
+        name: "Three",
+        image: "you.png",
+        lft: 5,
+        rgt: 10,
+        depth: 1
+    },
+    {
+        id: 4,
+        name: "Four",
+        image: "you.png",
+        lft: 6,
+        rgt: 7,
+        depth: 2
+    },
+    {
+        id: 5,
+        name: "Five",
+        image: "you.png",
+        lft: 8,
+        rgt: 9,
+        depth: 2
+    },
 ]
 
 export default function treeReducer(state=initialState, action) {
